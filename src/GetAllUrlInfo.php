629350a8fc71d92527d080bffe474aab35214d8e
18013272501@163.com <18013272501@163.com>
2021-07-30 18:48:02 +0800

    修改中间件

--- src/GetAllUrlInfo.php
+++ src/GetAllUrlInfo.php
@@ -2,11 +2,9 @@
 
 namespace Bucket;
 
-use Illuminate\Auth\Middleware\Authenticate as Middleware;
-
 use Bucket\Models\BucketRequest;
 
-class GetAllUrlInfo extends Middleware
+class GetAllUrlInfo
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
@@ -14,47 +12,46 @@ class GetAllUrlInfo extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
         return $next($request);
-    
     }
 
-    public function terminate($request, $response){
- //判断是否需要进行拦截分析
+    public function terminate($request, $response)
+    {
+        //判断是否需要进行拦截分析
         $uri = $request->getUri();
 
-        if(!in_array($uri, config('bucket.exceptUri'))){
-            return ;
+        if (!in_array($uri, config('bucket.exceptUri'))) {
+            return;
         }
 
         $this->createRequestInfo($request);
-
     }
 
-    protected function createRequestInfo(Request $request)
+    protected function createRequestInfo($request)
     {
         $endTime = microtime(true);
-        $params =[
+        $params = [
             'imei' => $this->getImei($request),
             'ip'   => $request->ip(),
-            'client' => CLIENT,
-            'fullUrl' => $request->getUri(), 
-            'api' => $request->getPathInfo(), 
+            'client' => 'ios',
+            'fullUrl' => $request->getUri(),
+            'api' => $request->getPathInfo(),
             'method' => $request->getMethod(),
             'timeIn' => date('Y-m-d H:i:s', LARAVEL_START),
-            'timeOut' => date('Y-m-d H:i:s', endTime),
-            'timeUsed' => date('Y-m-d H:i:s', endTime),
+            'timeOut' => date('Y-m-d H:i:s', $endTime),
+            'timeUsed' => $endTime - LARAVEL_START,
             'response' => json_encode(json_decode($response->getContent()), JSON_UNESCAPED_UNICODE)
-        ]; 
+        ];
         BucketRequest::create($params);
     }
 
 
 
-    protected  function getImei(Request $request)
+    protected  function getImei($request)
     {
-        
+
         $userAgent = $request->userAgent();
 
         $imeiKey = config('bucket.imei');
