3ceefd6a2f9a09fcce2b3df696ce235a02cbcfa6
18013272501@163.com <18013272501@163.com>
2021-08-03 16:24:28 +0800

    修改路由過濾規則，添加正則匹配

--- config/bucket.php
+++ config/bucket.php
@@ -4,7 +4,7 @@ $except_uri = [];
 $need_record_uri = [];
 
 return [
-    'except_uri' => $exceptUri,                  // 不需要记录的uri
+    'except_uri' => $except_uri,                  // 不需要记录的uri
     'except_pattern' => '',                    // 不需要積累的url的正則表達式
     'need_record_uri' => $need_record_uri,    // 不需要積累的url的正則表達式
     'need_record_pattern' => '',             // 不需要積累的url的正則表達式
